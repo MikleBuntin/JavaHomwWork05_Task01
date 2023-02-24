1f81c3da19d940d7abc38ef6b4ce886031b091d1
MikleBuntin <BuntinMA@mail.ru>
2023-02-24 19:59:54 +0300

    Готово!! :)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,7 +19,7 @@ public class Main {
                 break;
             }
             else if (str.equals("Add")) addPhone(phones);
-            else if (str.equals("Print")) printPhones();
+            else if (str.equals("Print")) printPhones(phones);
             else System.out.println("Неизвестная команда. Повторите ввод.");
         }
         System.out.println("Thanks!");
@@ -28,19 +28,24 @@ public class Main {
     public static void addPhone(Map<Integer, String> phones) {
         Scanner iScanner = new Scanner(System.in);
         System.out.println("Введите телефон-Имя");
-        String[] strArray = "-".split(iScanner.nextLine());
+//        String[] strArray = "-".split(iScanner.nextLine());
+        String str = iScanner.nextLine();
+//        System.out.println("str: " + str);
+        String[] strArray = str.split("-");
         Integer phone = Integer.parseInt(strArray[0]);
         String name = strArray[1];
 
         if (phones.containsKey(phone))
-            System.out.print("Такой телефон существует.");
-        else phones.put(phone,name);
-        iScanner.close();
-    }
-    public static void printPhones(Map<String, Integer> phones) {
-        for (var map : phones) {
+            System.out.println("Такой телефон существует.");
+        else {
+            phones.put(phone,name);
+            System.out.println("Телефон сохранён.");
+        }
 
-            System.out.print("Такой телефон существует.");
+    }
+    public static void printPhones(Map<Integer, String> phones) {
+        for (Integer phone : phones.keySet()) {
+            System.out.print("Phone: " + phone + ", Name: " + phones.get(phone));
         }
     }
 
